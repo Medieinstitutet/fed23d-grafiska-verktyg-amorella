3d29de208a0b3a17be439a099be845d52439779d
Diem Bang <diembang09@gmail.com>
2024-04-14 22:41:00 +0200

    :sparkles: make hamburger menu clickable

--- main.js
+++ main.js
@@ -1,1 +1,10 @@
 /* header + menu + nav */
+
+const hamMenu = document.querySelector(".hamMenu");
+
+const offScreenMenu = document.querySelector(".offScreenMenu");
+
+hamMenu.addEventListener("click", () => {
+  hamMenu.classList.toggle("active");
+  offScreenMenu.classList.toggle("active");
+});
